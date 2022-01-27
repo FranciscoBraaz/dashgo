2f06489be268f9da954f39180b64137b3ad3c1f3
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-01-27 19:47:58 -0300

    feat(index): criando tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,55 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
+
 export default function Home() {
-  return <h1>Dash.GO</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bgColor="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              type="email"
+              name="email"
+              bgColor="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              bgColor="gray.900"
+              variant="filled"
+              focusBorderColor="pink.500"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button colorScheme="pink" mt="6" type="submit" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
