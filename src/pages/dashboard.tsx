c76d6a0debc105771751c739e8fca005b674a992
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-01-27 23:03:43 -0300

    feat(SideBar): criando sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,16 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex flexDir="column">
+      <Header />
+
+      <Flex w="100%" maxW={1480} mx="auto" px="6" my="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
