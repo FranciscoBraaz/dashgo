278d5d8e2f2a26fb82b0089a162ee38f996f7ac2
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-02-06 18:54:27 -0300

    feat: adicionando lógica de paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -119,7 +119,11 @@ export default function Users() {
                   ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
