8fea1544734b75a95c3efd97aea87192ae2a1666
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-02-06 22:35:08 -0300

    fix: corrigindo rota da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
+let url = '';
+
+if (process.env.NODE_ENV === 'development') {
+  url = 'https://localhost:3000/api';
+} else {
+  url = 'dashgo-chi-seven.vercel';
+}
 
 export const api = axios.create({
-  baseURL: 'https://localhost:3000/api',
+  baseURL: url,
 });
